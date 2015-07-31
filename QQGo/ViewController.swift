04fb4c671a2b23b6a30b49080a5a46e6e6c36db5
Winston-liu-ray <harlequinliu@gmail.com>
2015-07-31 10:49:42 +0800

    test

---
+++ QQGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QQGo
+//
+//  Created by Ｗinston on 5/13/15.
+//  Copyright (c) 2015 Ｗinston. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+}
